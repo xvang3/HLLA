fabcd28597388f0b021d59c48aba4dd07946ab42
Gaozong Lo <logaozong@gmail.com>
2024-10-23 23:45:50 -0800

    working setup for navigation bar and html pages

--- hmoob_lus/main/urls.py
+++ hmoob_lus/main/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
     path('audio-test/', views.play_audio_test, name='audio_test'),  # Ensure this exists
     path('', views.home, name='home'),  # Home page view
+    path('about/', views.about, name='about'),  # About page
+    path('courses/', views.courses, name='courses'),  # Course page
+    path('practice/', views.practice, name='practice'),  # Practice page
+    path('resources/', views.resources, name='resources'),  # Resources page
+
     # Add other URLs as needed
 ]
