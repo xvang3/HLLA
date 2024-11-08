80fe567eca9b7c23e529c8c5a0b4393ece33dde3
Khafaniking <80512615+Khafaniking@users.noreply.github.com>
2024-11-08 01:01:52 -0900

    Update urls.py
    Point to local file path of the .wav files

--- hmoob_lus/main/urls.py
+++ hmoob_lus/main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('audio-test/', views.play_audio_test, name='audio_test'),  # Ensure this exists
@@ -10,4 +12,4 @@ urlpatterns = [
     path('resources/', views.resources, name='resources'),  # Resources page
 
     # Add other URLs as needed
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
