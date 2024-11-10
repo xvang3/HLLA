aa4fa29ef81b54de7213f095b584d10514a4058a
SylverEnigma <shanellealmario@gmail.com>
2024-11-09 16:39:35 -0900

    changed html file names and added folders for organization

--- hmoob_lus/main/urls.py
+++ hmoob_lus/main/urls.py
@@ -9,21 +9,22 @@ urlpatterns = [
     path('practice/', views.practice, name='practice'),  # Practice page
     path('resources/', views.resources, name='resources'),  # Resources page
     
-    path('units/tones/', views.vowels_consonants, name='u1_tones'), # Course Units
-    path('units/vowels_and_consonants/', views.vowels_consonants, name='vowels_consonants'),
-    path('units/vowels/', views.vowels_consonants, name='u2_vowels'),
-    path('units/consonants/', views.vowels_consonants, name='u3_consonants'),
-    path('units/phrases/', views.phrases, name='u4_phrases'),
-    path('units/numbers/', views.numbers, name='u5_numbers'),
-    path('units/family/', views.family, name='u6_family'),
-    path('units/animals/', views.animals, name='u7_animals'),
+    path('units/u1_tones/', views.u1_tones, name='u1_tones'), # Course Units
+    path('units/u2_vowels_and_consonants/', views.u2_vowels_consonants, name='u2_vowels_consonants'),
+    path('units/u2_vowels/', views.u2_vowels, name='u2_vowels'),
+    path('units/u2_consonants/', views.u2_consonants, name='u2_consonants'),
+    path('units/u3_phrases/', views.u3_phrases, name='u3_phrases'),
+    path('units/u4_numbers/', views.u4_numbers, name='u4_numbers'),
+    path('units/u5_family/', views.u5_family, name='u5_family'),
+    path('units/u6_animals/', views.u6_animals, name='u6_animals'),
 
-    path('practice/tones/', views.tones, name='p1_tones'), # Practice Units
-    path('practice/vowels/', views.vowels, name='p2_vowels'),
-    path('practice/consonants/', views.consonants, name='p3_consonants'),
-    path('practice/phrases/', views.phrases, name='p4_phrases'),
-    path('practice/numbers/', views.numbers, name='p5_numbers'),
-    path('practice/family/', views.family, name='p6_family'),
-    path('practice/animals/', views.animals, name='p7_animals'),
+    path('practice/p1_tones/', views.p1_tones, name='p1_tones'), # Practice Units
+    path('practice/p2_vowels_and_consonants/', views.u2_vowels_consonants, name='p2_vowels_consonants'),
+    path('practice/p2_vowels/', views.p2_vowels, name='p2_vowels'),
+    path('practice/p2_consonants/', views.p2_consonants, name='p2_consonants'),
+    path('practice/p3_phrases/', views.p3_phrases, name='p3_phrases'),
+    path('practice/p4_numbers/', views.p4_numbers, name='p4_numbers'),
+    path('practice/p5_family/', views.p5_family, name='p5_family'),
+    path('practice/p6_animals/', views.p6_animals, name='p6_animals'),
     # Add other URLs as needed
 ]
--- hmoob_lus/main/views.py
+++ hmoob_lus/main/views.py
@@ -25,78 +25,60 @@ def u1_tones(request):
     words = Word.objects.filter(category="Tones")
     return render(request, 'units/u1_tones.html', {'words': words})
 
-def vowels_consonants(request):
-    return render(request, 'vowels_consonants.html')
+def u2_vowels_consonants(request):
+    return render(request, 'units/u2_vowels_consonants.html')
 
 def u2_vowels(request):
     words = Word.objects.filter(category="Vowels")
     return render(request, 'units/u2_vowels.html', {'words': words})
 
-def u3_consonants(request):
+def u2_consonants(request):
     words = Word.objects.filter(category="Consonants")
-    return render(request, 'units/u3_consonants.html', {'words': words})
+    return render(request, 'units/u2_consonants.html', {'words': words})
 
-<<<<<<< HEAD
-def u4_phrases(request):
+def u3_phrases(request):
     words = Word.objects.filter(category="Common Phrases")
-    return render(request, 'units/u4_phrases.html', {'words': words})
-=======
-def tones(request):
-    words = Word.objects.filter(category="Tones")
-    return render(request, 'tones.html', {'words': words})
+    return render(request, 'units/u3_phrases.html', {'words': words})
 
-def vowels_consonants(request):
-    return render(request, 'vowels_consonants.html')
-
-def vowels(request):
-    words = Word.objects.filter(category="Vowels")
-    return render(request, 'vowels.html', {'words': words})
-
-def consonants(request):
-    words = Word.objects.filter(category="Consonants")
-    return render(request, 'consonants.html', {'words': words})
-
-def animals(request):
-    words = Word.objects.filter(category="Animals")
-    return render(request, 'animals.html', {'words': words})
->>>>>>> b5d8380c34d9810d4f4f3caffc6eabf042ccd5f3
-
-def u5_numbers(request):
+def u4_numbers(request):
     words = Word.objects.filter(category="Numbers")
-    return render(request, 'units/u5_numbers.html', {'words': words})
+    return render(request, 'units/u4_numbers.html', {'words': words})
 
-def u6_family(request):
+def u5_family(request):
     words = Word.objects.filter(category="Family")
-    return render(request, 'units/u6_family.html', {'words': words})
+    return render(request, 'units/u5_family.html', {'words': words})
 
-def u7_animals(request):
+def u6_animals(request):
     words = Word.objects.filter(category="Animals")
-    return render(request, 'units/u7_animals.html', {'words': words})
+    return render(request, 'units/u6_animals.html', {'words': words})
 
 def p1_tones(request):
     words = Word.objects.filter(category="Tones")
     return render(request, 'practice/p1_tones.html', {'words': words})
 
+def p2_vowels_consonants(request):
+    return render(request, 'practice/p2_vowels_consonants.html')
+
 def p2_vowels(request):
     words = Word.objects.filter(category="Vowels")
     return render(request, 'practice/p2_vowels.html', {'words': words})
 
-def p3_consonants(request):
+def p2_consonants(request):
     words = Word.objects.filter(category="Consonants")
-    return render(request, 'practice/p3_consonants.html', {'words': words})
+    return render(request, 'practice/p2_consonants.html', {'words': words})
 
-def p4_phrases(request):
+def p3_phrases(request):
     words = Word.objects.filter(category="Common Phrases")
-    return render(request, 'practice/p4_phrases.html', {'words': words})
+    return render(request, 'practice/p3_phrases.html', {'words': words})
 
-def p5_numbers(request):
+def p4_numbers(request):
     words = Word.objects.filter(category="Numbers")
-    return render(request, 'practice/p5_numbers.html', {'words': words})
+    return render(request, 'practice/p4_numbers.html', {'words': words})
 
-def p6_family(request):
+def p5_family(request):
     words = Word.objects.filter(category="Family")
-    return render(request, 'practice/p6_family.html', {'words': words})
+    return render(request, 'practice/p5_family.html', {'words': words})
 
-def p7_animals(request):
+def p6_animals(request):
     words = Word.objects.filter(category="Animals")
-    return render(request, 'practice/p7_animals.html', {'words': words})
+    return render(request, 'practice/p6_animals.html', {'words': words})
