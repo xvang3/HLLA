5d697bef4daa10d856b7b29b2c3183c37c262510
Xue Vang <ntxhwvaj49@gmail.com>
2024-12-06 08:24:04 -0900

    Cleaned up some files and updated new family images

--- hmoob_lus/analyze.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Load the Excel file to inspect its content
-excel_path = 'C:\\Users\\ntxhw\\Downloads\\PDF HLLA.xlsx'
-df_excel = pd.read_excel(excel_path)
-
-# Display the first few rows to understand its structure
-df_excel.head()
--- hmoob_lus/main/management/commands/clear_database.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from main.models import Word
-
-class Command(BaseCommand):
-    help = 'Clear the Word table'
-
-    def handle(self, *args, **kwargs):
-        Word.objects.all().delete()
-        self.stdout.write('All words deleted successfully.')
